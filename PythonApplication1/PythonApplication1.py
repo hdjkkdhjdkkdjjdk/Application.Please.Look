59155471435a9ff96fe63abee5fdbb5683412e7f
drgen <drgen@MSI>
2024-05-19 18:29:45 +0300

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,32 @@
+def main():
+    while True:
+        input("Hello And welcome to THE SHOP(By Amad Ismail Muhammad Jalal Mahmud Ahmad)(Press enter to continue for every single line((If it's not a question.))")
+        input('So')
+        input('We have:\nchatgpt (out of stock)\nside business tutor\nmy life ')
+
+        thing_they_ordered = input("What would you like? (Don't answer this in caps (And don't order 2 things!)) (if you don't answer you'll get sent to the start! ")
+
+        if thing_they_ordered == 'side business tutor':
+            doolar = input("That would be 10$ (if you give me the money type *gives 10$*) AND IF YOU DON'T I AM SENDING YOU TO THE START ")
+            if doolar == '*gives 10$*':
+                print('Starting...')
+                input("Let's get going!'")
+                input("Side businesses are an important thing in life")
+                input('To start, Learn any coding language -- But before that you should Think about how to publish apps and such in your coding')
+                input('Then (if you have a mac) go to Xcode and publish your work but it will be 100$ to publish it')
+                input("But if you have Windows, find a way to publish your work")
+                input('Then wait!!!')
+                break  # End it
+        elif thing_they_ordered == 'my life':
+            doolr = input("That would be 1$ (if you give me the money type *gives 1$*) AND IF YOU DON'T I AM SENDING YOU TO THE START ")
+            if doolr == '*gives 1$*': print("This one was a scam (but the others weren't)")
+            input("you won't choose another one because i don't want you to so let's skip to the next part! ")
+            break  # End it
+        elif thing_they_ordered == 'chatgpt':
+            print('Bro pick it again')
+
+if __name__ == "__main__":
+    main()
+
+print('Thank you, Code avengers! (()()()()(((())((I learnt this from code avengers!')
+print("I used py charm i am in unit 12 or 13")
